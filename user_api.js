a3d60e12703d4d0bd7e66fbb812cbd2f97137a54
Obscuritier-Chen <133127004+Obscuritier-Chen@users.noreply.github.com>
2024-05-26 22:19:10 +0800

    Add files via upload

--- user_api.js
+++ user_api.js
@@ -1,4 +1,9 @@
 let uid;
+let Username;
+function ownUserPageCreate()
+{
+
+}
 window.addEventListener('DOMContentLoaded',()=>{
     //md不用了
     //const pattern=/user\/(\d+)/;//正则表达式 获取user/后面的数字部分
@@ -17,13 +22,19 @@ window.addEventListener('DOMContentLoaded',()=>{
         console.error(error);
     });
 
-    fetch(`${window.location.href}api/get_user_info`,{//页面用户的名字
+    fetch(`${window.location.href}api/get_user_info`,{//页面用户的名字 user/n/api/get_user_info
         method: 'GET',
     })
     .then(response=>response.json())
     .then(data=>{
         //console.log(data.username);
         document.title=data.username;
+        console.log(data.signature)
+        document.getElementsByClassName('topUsername')[0].innerText=data.username;
+        /*if(data.username==Username)//用户自己的主页
+        {
+
+        }*/
     })
     .catch(error=>{
         console.error(error);
