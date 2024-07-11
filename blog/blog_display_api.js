95311fedc9fc0080c9fcd5fcd75baf48c1017c50
Obscuritier-Chen <133127004+Obscuritier-Chen@users.noreply.github.com>
2024-07-12 01:59:31 +0800

    Add files via upload

--- blog/blog_display_api.js
+++ blog/blog_display_api.js
@@ -1,24 +1,3 @@
-function blogDisplay(data)
-{
-    fetch(`${window.location.origin}/user/${data.uid}/api/get_user_info/`,{
-        method: 'GET',
-    })
-    .then(response=>response.json())
-    .then(data=>{
-        document.getElementsByClassName('blogAuthorName')[0].innerText=data.username;
-    })
-    .catch(error=>{
-        console.error(error);
-    });
-
-    const blogPublishTime=`${data.year}年${data.month}月${data.day}日 ${data.hour}:${data.minute}`;
-    document.getElementsByClassName('blogPublishTime')[0].innerText=blogPublishTime;
-
-    document.getElementsByClassName('blogViewNum')[0].innerText=data.view_num;
-
-    const blogContent=document.getElementsByClassName('blogContent')[0];
-    Vditor.preview(blogContent, data.content, {cdn: 'https://cdn.jsdelivr.net/npm/vditor@3.8.14'});
-}
 window.addEventListener('DOMContentLoaded',()=>{
     fetch(`${window.location.href}/api/get_blog_data/`,{
         method: 'GET',
@@ -34,23 +13,40 @@ window.addEventListener('DOMContentLoaded',()=>{
         console.error(error);
     });
 });
-const tip=document.createElement('div');
-tip.className='likeGivenTip';
-tip.innerText='取消点赞';
-document.getElementsByClassName('giveBlogLike')[0].addEventListener('click',()=>{
-    const element=document.getElementsByClassName('giveBlogLike')[0];
-    document.getElementsByClassName('giveBlogLikeText')[0].innerText=element.classList.contains('likeGiven') ? '点赞' : '已点赞';
-    if(element.classList.contains('likeGiven'))
-        tip.remove();
-    element.classList.toggle('likeGiven');//有就删 没就添
-});
-document.getElementsByClassName('giveBlogLike')[0].addEventListener('mouseover',()=>{
-    const element=document.getElementsByClassName('giveBlogLike')[0];
-    if(element.classList.contains('likeGiven'))
-        element.parentNode.appendChild(tip);
-});
-document.getElementsByClassName('giveBlogLike')[0].addEventListener('mouseleave',()=>{
-    const element=document.getElementsByClassName('giveBlogLike')[0];
-    if(element.classList.contains('likeGiven'))
-        tip.remove();
-});
+function likeGiveOrDelete()
+{
+    fetch(`${window.location.href}/api/give_delete_like/`,{
+        method: 'POST',
+    })
+    .then(response=>response.json())
+    .then(data=>{
+        if(data.message=='blog like successfully'||data.message=='blog like delete successfully')
+        {
+            document.getElementsByClassName('giveBlogLikeNum')[0].innerText=data.like_num;
+            document.getElementsByClassName('blogLikeNum')[0].innerText=data.like_num;
+        }
+    })
+    .catch(error=>{
+        console.error(error);
+    });
+}
+document.getElementsByClassName('submitCommentButton')[0].addEventListener('click',()=>{
+    const content=document.getElementsByClassName('submitComment')[0].value;
+    if(content.length>200||content.length==0)
+        return;
+
+    let form=new FormData();
+    form.append('content', content);
+
+    fetch(`${window.location.href}/api/create_blog_comment/`,{
+        method: 'POST',
+        body: form//JSON.stringify({content: content}),
+    })
+    .then(response=>response.json())
+    .then(data=>{
+        console.log(data);
+    })
+    .catch(error=>{
+        console.error(error);
+    });
+})
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.views.generic.base import TemplateView
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import get_user_model
-from .models import Blogs
+from .models import Blogs, BlogsLike, BlogsComment
 import json
 
 
@@ -35,10 +35,18 @@ def blog_publish(request):
 
 @csrf_exempt
 def get_blog_data(request, blog_id):
+    user = request.user
     try:
         blog = Blogs.objects.get(blog_id=blog_id)
         blog.view_num += 1
         blog.save()
+
+        try:
+            like = BlogsLike.objects.get(blog_id=blog_id, user=user)
+            like_status = False#不提前声明就用好奇怪
+        except BlogsLike.DoesNotExist:
+            like_status = True
+
         response_data = {
             'uid': blog.user_uid,
             'blog_title': blog.blog_title,
@@ -48,6 +56,8 @@ def get_blog_data(request, blog_id):
             'hour': blog.publish_time.hour,
             'minute': blog.publish_time.minute,
             'view_num': blog.view_num,
+            'like_num': blog.like_num,
+            'like_status': like_status,
             'content': blog.blog_content,
             'message': 'get blog successfully',
         }
@@ -55,3 +65,55 @@ def get_blog_data(request, blog_id):
     except Blogs.DoesNotExist:
         return JsonResponse({'message': 'blog not found'}, status=404)
 
+
+@csrf_exempt
+def give_delete_like(request, blog_id):
+    if request.method == 'POST':
+        user = request.user
+        try:
+            blog = Blogs.objects.get(blog_id=blog_id)
+            like, created = BlogsLike.objects.get_or_create(user=user, blog=blog)
+
+            if created:
+                blog.like_num += 1
+                blog.save()
+                return JsonResponse({'message': 'blog like successfully', 'like_num': blog.like_num})
+            else:
+                like.delete()
+                blog.like_num -= 1
+                blog.save()
+                return JsonResponse({'message': 'blog like delete successfully', 'like_num': blog.like_num})
+        except Blogs.DoesNotExist:
+            return JsonResponse({'message': 'blog not found'}, status=404)
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
+
+
+@csrf_exempt
+def create_blog_comment(request, blog_id):
+    if request.method == 'POST':
+        user = request.user
+        content = request.POST.get('content')
+
+        if len(content) >= 200 or len(content) <= 0:
+            return JsonResponse({'message': 'illegal comment'})
+        try:
+            blog = Blogs.objects.get(blog_id=blog_id)
+            comment = BlogsComment.objects.create(
+                blog=blog,
+                user=user,
+                content=content,
+            )
+            response_data = {
+                'message': 'comment created successfully',
+                'comment_id': comment.comment_id,
+                'comment_time': comment.comment_time,
+                'content': comment.content,
+            }
+            return JsonResponse(response_data)
+
+        except Blogs.DoesNotExist:
+            return JsonResponse({'message': 'blog not found'}, status=404)
+
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
