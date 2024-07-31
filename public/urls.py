ac7e69524dc0c6174aaa4d6f726677ce1fc55a1a
Obscuritier-Chen <133127004+Obscuritier-Chen@users.noreply.github.com>
2024-07-31 20:49:02 +0800

    Delete public directory

--- public/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.PublicView.as_view(), name='public'),
-    path('api/submit_public_chat/', views.submit_public_chat, name='submit-public-chat'),
-]
