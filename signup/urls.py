49595eeee227b3ad7d72ac8f4c0b66de241103ac
Obscuritier-Chen <133127004+Obscuritier-Chen@users.noreply.github.com>
2024-07-31 20:49:14 +0800

    Delete signup directory

--- signup/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.SignupView.as_view(), name='signup'),
-    path('post-new-user/', views.handle_signup_post, name='post-new-user'),
-]
